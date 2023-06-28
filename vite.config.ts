fc9366ac76ead813350e27ee3af12d4be60e2529
vkpatel007 <vkpatel007@gmail.com>
2023-06-28 18:12:39 -0400

    final boilerplate
    Co-authored-by: cronullarc <cronullarc@users.noreply.github.com>
    Co-authored-by: Philip Brown <starfishpanda@users.noreply.github.com>
    Co-authored-by: George Greer <ggreer91@users.noreply.github.com>
    Co-authored-by: NikoAmescua <NikoAmescua@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+const PORT = process.env.PORT || 3000;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      }
+    },
+  },
+  plugins: [svelte()],
+})
