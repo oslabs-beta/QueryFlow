a413873f472d2e0232392da6ea682093fc209754
vkpatel007 <vkpatel007@gmail.com>
2023-06-29 00:40:36 -0400

    add signup and login
    Co-authored-by: George Greer <ggreer91@users.noreply.github.com>
    Co-authored-by: NikoAmescua <NikoAmescua@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,24 @@
-<script lang="ts">
-	
-  import '@skeletonlabs/skeleton/styles/skeleton.css';
-
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
-	// control rendering login OR signup within slot
-	let renderLoginPage: boolean = true;
-  
+<script>
+	import LoginPage from './lib/Login.svelte'
+	import Navbar from './lib/Navbar.svelte'
+	import Signup from './lib/Signup.svelte'
+  import graph from './assets/graph.jpg'
 </script>
 
-<AppShell>
+<div class="container mx-auto">
+  <Navbar/>
+  <div class="grid grid-cols-2 gap-2">
+		<div class="h-3/6">
+      <img src={graph} alt="">
+    </div>
+    <LoginPage />
+		<Signup />
+
+  </div>
+</div>
+
+
+
+<style>
   
-	<svelte:fragment slot="header">
-    <AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">QueryFlow</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-
-
-  </svelte:fragment>
-	<!-- (sidebarLeft) -->
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
-	<!-- <slot /> -->
-	<!-- ---- / ---- -->
-	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-	<!-- (footer) -->
-</AppShell>
-
-<!-- <style>
-  * {
-		display: flex;
-	}
-</style> -->
+</style>
---
+++ src/lib/Signup.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<div class="relative flex flex-col justify-center h-screen overflow-hidden">
+  <div class="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-lg">
+      <h1 class="text-3xl font-semibold text-center text-gray-700">Sign Up</h1>
+      <form class="space-y-4">
+          <div>
+            <label for="email" class="label">
+              <span class="text-base label-text">Email</span>
+              </label>
+              <input type="text" placeholder="Email Address" class="w-full input input-bordered" />
+          </div>
+          <div>
+              <label for="password" class="label">
+                  <span class="text-base label-text">Password</span>
+              </label>
+              <input type="password" placeholder="Enter Password"
+                  class="w-full input input-bordered" />
+          </div>
+          <div>
+              <button class="btn btn-block">Sign Up</button>
+          </div>
+      </form>
+  </div>
+</div>
+
+<style>
+.input {
+    background-color: lightgray;
+  }
+</style>
