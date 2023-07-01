cd9ae28846f93b1b6c9df3b84119d89aeb6e5f2f
Niko <niko8774@yahoo.com>
2023-06-30 21:25:30 -0700

    sync authentication with backend
    Co-authored-by: cronullarc <cronullarc@users.noreply.github.com>
    Co-authored-by: George Greer <ggreer91@users.noreply.github.com>
    Co-authored-by: Vivek Patel <vkpatel007@users.noreply.github.com>
    Co-authored-by: Philip Brown <starfishpanda@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,23 @@
-<script>
-	import LoginPage from './lib/Login.svelte'
-	import Navbar from './lib/Navbar.svelte'
-	import Signup from './lib/Signup.svelte'
-  import { Route, Router } from 'svelte-routing';
-  import About from './Pages/About.svelte';
-  import Contact from './Pages/Contact.svelte';
-  import Home from './Pages/Home.svelte';
-  import Landing from './Pages/Landing.svelte';
-  import Tips from './Pages/Tips.svelte';
-
+<script lang="ts">
+	import Navbar from './lib/Navbar.svelte';
+	import { Route, Router } from 'svelte-routing';
+	import About from './Pages/About.svelte';
+	import Contact from './Pages/Contact.svelte';
+	import Home from './Pages/Home.svelte';
+	import Landing from './Pages/Landing.svelte';
+	import Tips from './Pages/Tips.svelte';
 </script>
 
-
-<main class="md:h-screen">
-  <Navbar/>
-  <Router>
-    <Route path="/" component={Landing} />
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact} />
-    <Route path="/tips" component={Tips} />
-    <Route path="/home" component={Home} />
-  </Router>
+<main>
+	<Navbar />
+	<Router>
+		<Route path="/" component={Landing} />
+		<Route path="/about" component={About} />
+		<Route path="/contact" component={Contact} />
+		<Route path="/tips" component={Tips} />
+		<Route path="/homepage" component={Home} />
+	</Router>
 </main>
 
-
 <style>
-  
 </style>
