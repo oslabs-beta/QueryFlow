e6deffbfad701f0bd4d3d2a8301f41db8b1b52a7
ggreer91 <georgegreer91@gmail.com>
2023-07-06 15:19:39 -0700

    Add navbar tags, temporarily comments 3-dot button

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -10,23 +10,27 @@
       QueryFlow
     </a>
   </div>
-  <div class="flex items-spacebe">
-    <a href="$">About</a>
-    <a href="$">SQL P Tips</a>
-    <a href="$">QF GitHub</a>
-    <a href="$">Logout</a>
-    <!-- 3-dot button in top-right corner -->
-    <button class="btn btn-square btn-ghost">
+  <div class="flex-none">
+    <a class="navTags" href="$">About</a>
+    <a class="navTags" href="$">SQL P Tips</a>
+    <a class="navTags" href="$">QF GitHub</a>
+    <a class="navTags" href="$">Logout</a>
+    <!-- 3-dot button in top-right corner, replace all navTags with this on smaller screens -->
+    <!-- <button class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z">
         </path>
       </svg>
-    </button>
+    </button> -->
   </div>
 </div>
 
 <style>
   .navbar {
     border-bottom: 1px solid #cecece;
+  }
+
+  .navTags {
+    margin-right: 25px;
   }
 </style>
