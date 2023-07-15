bcccef83621e45a554fb7f790bc166b201d858a5
Niko <niko8774@yahoo.com>
2023-07-14 20:11:11 -0700

    update tips and about pages

--- src/Pages/Landing.svelte
+++ src/Pages/Landing.svelte
@@ -3,47 +3,48 @@
 	import Signup from '../lib/Signup.svelte';
 	import graph from '../assets/graph3.png';
 	import { navigate } from 'svelte-routing';
-  import { onMount } from 'svelte';
-  // toggle signup/login component
+	import { onMount } from 'svelte';
+	// toggle signup/login component
 	let renderSignup: boolean = false;
 
-  
-  onMount(async () => {
-    if (localStorage.getItem('token')) navigate('/home', { replace:true });
-  })
-  
+	onMount(async () => {
+		if (localStorage.getItem('token')) navigate('/home', { replace: true });
+	});
 </script>
 
 <div class="landing-container flex flex-col mt-5 lg:flex-row">
-	<div class="flex-1 flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 responsive-column">
-		<img class="landing-img w-full h-auto" src={graph} alt="graph" />
+	<div
+		class="flex-1 flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 responsive-column"
+	>
+		<!-- <img class="landing-img w-full h-auto" src={graph} alt="graph" /> -->
+		<img class="object-contain rounded-lg" src={graph} alt="graph" />
 	</div>
-  <!-- conditional statement to render signup or login -->
-		{#if renderSignup}
-			<Signup bind:renderSignup />
-		{:else}
-			<Login bind:renderSignup />
-		{/if}
+	<!-- conditional statement to render signup or login -->
+	{#if renderSignup}
+		<Signup bind:renderSignup />
+	{:else}
+		<Login bind:renderSignup />
+	{/if}
 </div>
 
 <style>
-  .landing-img {
-    width: 100%;
-    height: 100%;
-    object-fit: fit;
-    border-radius: 15px; 
-  }
-  
-  .landing-container{
-    height:80vh
-  }
-  .responsive-column{
-    height:90vh;
-  }
+	.landing-img {
+		width: 100%;
+		height: 100%;
+		object-fit: fit;
+		border-radius: 15px;
+	}
 
-  @media only screen and (max-width: 1025px) {
-    .landing-img {
-      display: none;
-    }
-  }
+	.landing-container {
+		height: 80vh;
+	}
+	.responsive-column {
+		height: 90vh;
+	}
+
+	@media only screen and (max-width: 1025px) {
+		.landing-img {
+			display: none;
+		}
+	}
 </style>
--- src/Pages/Tips.svelte
+++ src/Pages/Tips.svelte
@@ -3,7 +3,7 @@
 
 <article class="mt-10 m-10 pb-10 mb-0 prose md:mx-20 lg:mx-40 xl:mx-60">
 	<h1>PostgreSQL database efficiency</h1>
-	<p>Below are some general options mitigate slow performing queries</p>
+	<p>Below are some general options for mitigating slow-performing queries:</p>
 	<h3>Use Indexes Wisely</h3>
 	<p>
 		Proper indexing can greatly improve query performance. Identify the columns frequently used in
@@ -13,7 +13,7 @@
 	</p>
 	<h3>Optimize Joins</h3>
 	<p>
-		select appropriate join algorithms (nested loop, hash join, merge join) based on the size of the
+		Select appropriate join algorithms (nested loop, hash join, merge join) based on the size of the
 		tables, available indexes, and join conditions. Ensure the join columns have proper indexes, and
 		consider restructuring the query or breaking it into smaller parts using subqueries or Common
 		Table Expressions (CTEs) to optimize performance.
@@ -21,14 +21,14 @@
 	<h3>Avoid Overfetching</h3>
 	<p>
 		Fetch only the necessary data in your queries. Look for places where queries are fetching
-		columns or rows that aren’t required. Using proper filtering and projection to retrieve only the
-		relevant data will reduce network traffic and improve query performance.
+		columns or rows that are not required. Using proper filtering and projection to retrieve only
+		the relevant data will reduce network traffic and improve query performance.
 	</p>
 	<h3>Refactor your Query Strings</h3>
 	<p>
 		Review your queries and consider refactoring them. Look for complex subqueries, unnecessary
-		joins or redundant conditions. Additionally, restructuring the query logic can sometimes achieve
-		the same result with more efficiency.
+		joins, or redundant conditions. Additionally, restructuring the query logic can sometimes
+		achieve the same result with more efficiency.
 	</p>
 	<h3>Consider Denormalization</h3>
 	<p>
@@ -41,6 +41,6 @@
 	<p>
 		Caching in a secondary database is an ideal option for fast data retrieval. Especially for
 		queries where SQL optimization is not feasible. Caching is configurable for most SQL databases
-		but geocaching is another option. See the QueryFlow auto caching engine.
+		but geocaching is another option. See the QueryFlow auto-caching engine.
 	</p>
 </article>
