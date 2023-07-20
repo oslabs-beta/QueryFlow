f2baecedeae4b4abbb73985a969e92ebf4f28f81
vkpatel007 <vkpatel007@gmail.com>
2023-07-20 13:34:02 -0400

    add server.mjs image content types
    Co-authored-by: Niko Amescua <NikoAmescua@users.noreply.github.com>
    Co-authored-by: Philip Brown <starfishpanda@users.noreply.github.com>

--- server/server.mjs
+++ server/server.mjs
@@ -24,6 +24,17 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+app.use('/dist', (req, res, next) => {
+  if (req.path.endsWith('.png')) {
+    res.setHeader('Content-Type', 'image/png');
+  } else if (req.path.endsWith('.jpg') || req.path.endsWith('.jpeg')) {
+    res.setHeader('Content-Type', 'image/jpeg');
+  } else if (req.path.endsWith('.avif')) {
+    res.setHeader('Content-Type', 'image/avif');
+  } 
+  next();
+});
+
 // Serve static assets
 // Change to '../src' if problem. This current setup is for distribution
 app.use(express.static(resolve(__dirname, '../dist')));
