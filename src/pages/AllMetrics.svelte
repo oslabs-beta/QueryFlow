4ab21750da08a23ab1ab374a0e50989b17e056c5
vkpatel007 <vkpatel007@gmail.com>
2023-07-20 15:00:04 -0400

    rework all metrics

--- src/pages/AllMetrics.svelte
+++ src/pages/AllMetrics.svelte
@@ -4,78 +4,84 @@
 	import { metricData } from '../store';
 	import type { QueryData, WorkingArr, Directions } from '../types';
 	import type { ScaleBand, ScaleLinear } from 'd3';
-	import AxisX from '../graphs/graph-components/AxisX.svelte';
-	import AxisY from '../graphs/graph-components/AxisY.svelte';
-	import Tooltip from '../graphs/graph-components/Tooltip.svelte';
 	import { navigate } from 'svelte-routing';
 	import RedisForm from '../lib/RedisForm.svelte';
 	import { Modal } from 'flowbite-svelte';
-
+  import { onMount,tick } from 'svelte';
 	// reactive metrics array
 	$: metrics = [];
 	
 	metricData.subscribe((data: QueryData[]) => {
 		metrics = data;
+		console.log(metrics)
 	});
 
 	let formModal: boolean = false;
 
-	// declaring optional params for later defining workingArr with proper keys in obj elements
-	// data conversion for bar chart
-	const format: Function = (data: QueryData[], buckets: number): WorkingArr[] => {
-		const workingArr: WorkingArr[] = [];
 
-		// finds range of values
-		const highVal = data.reduce((acc: number, obj: QueryData) => Math.max(obj.averageTime, acc), 0);
+	// format data
+  let data = metrics.sort((a, b) => a.averageTime - b.averageTime).map((el, i) => {
+		return {queryName: el.queryName.concat(i > 0 ? `-${i}` : ""), averageTime: el.averageTime}
+	})
 
-		// divides range by buckets
-		for (let i = 0; i < buckets; i++) {
-			workingArr.push({});
-			workingArr[i].topValue = Math.ceil(((i + 1) * highVal) / buckets);
-			workingArr[i].bottomValue = workingArr[i - 1] ? workingArr[i - 1].topValue : 0;
-			workingArr[i].numberOfQueries = 0;
+  let tooltipContent = "";
+  let tooltipX = 0;
+  let tooltipY = 0;
+  let showTooltip = false;
+
+  let textNode;  // reference to the text node
+  let rectWidth = 0;  // will store the width of the rectangle
+
+  function handleMouseOver(d, event) {
+    showTooltip = true;
+    tooltipContent = `Query: ${d.queryName} - Time: ${d.averageTime}`;
+    tooltipX = xScale(d.averageTime) + 10;  // Positioning the tooltip to the right of the bar
+    tooltipY = yScale(d.queryName) + yScale.bandwidth() / 2;
+
+    // Request an update after changing tooltipContent
+    tick().then(() => {
+      if (textNode) {
+        const bbox = textNode.getBBox();
+        rectWidth = bbox.width + 10;  // add some padding
+      }
+    });
+  }
+
+  function handleMouseOut() {
+    showTooltip = false;
+  }
+let width:any, height:any;
+	const margin = { top: 20, right: 20, bottom: 20, left: 180 };
+
+	onMount(() => {
+		const resize = () => {
+			const container = document.querySelector(".h-full.w-full");
+			width = container.clientWidth;
+			height = container.clientHeight;
 		}
+		window.addEventListener('resize', resize);
+		resize();
 
-		// adds each value to numberOfQueries
-		for (const dataPoint of data) {
-			for (let i = 0; i < workingArr.length; i++) {
-				if (workingArr[i].topValue >= dataPoint.averageTime) {
-					workingArr[i].numberOfQueries += 1;
-					break;
-				}
-			}
-		}
-		return workingArr;
-	};
-
-  
+		return () => {
+			window.removeEventListener('resize', resize);
+		};
+	});
 
 
-	// calculating buckets dynamically to limit number of buckets with smaller/larger data sizes
-	// VERY IMPORTANT!
-	let buckets = metrics.length > 4 ? Math.ceil(Math.log2(metrics.length - 3) * 1.7) : 2;
-	if (!metrics.length) buckets = 0;
+	$: if (width && height) {
+		const innerHeight = height - margin.top - margin.bottom ;
+		const innerWidth = width - margin.left - margin.right;
 
+		yScale.range([0, innerHeight]);
+		xScale.range([0, innerWidth]);
+	}
 
-	// formatting data
-	const barChartData: WorkingArr[] = format(metrics, buckets);
+	$: xDomain = data.map((d) => d.queryName);
+	$: yDomain = data.map((d) => +d.averageTime);
 
-	let width: number = 600;
-	let height: number = 600;
-	const margin: Directions = { top: 20, right: 40, left: 40, bottom: 25 };
-
-  
-	const xScale: ScaleBand<string> = scaleBand()
-		.domain(barChartData.map((_, i) => `${i}`))
-		.range([margin.left, width - margin.right])
-		.padding(0.1);
-
-	const yScale: ScaleLinear<number, number> = scaleLinear()
-		.domain([0, max(barChartData, (d) => d.numberOfQueries)])
-		.range([height - margin.top - margin.bottom, 0]);
-
-	let hoveredData: WorkingArr;
-	
+	$: yScale = scaleBand().domain(xDomain).range([0, height]).padding(0.1);
+	$: xScale = scaleLinear().domain([0, Math.max.apply(null, yDomain)]).range([0, width]);
+  $: innerHeight = height - margin.top - margin.bottom ;
 </script>
 
 <!-- button to return to home page -->
@@ -94,89 +100,85 @@
 		<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
 	</svg>
 </button>
+<div class="flex justify-center mb-8">
+  <h1 class="title">Average Query Times</h1>
 
-<!-- renders chart if at least 1 query exists -->
-{#if buckets}
-	<div class="chart-container" bind:clientWidth={width}
-		on:mouseleave={() => {
-			hoveredData = null;
-		}}>
-		<div class="flex justify-center mb-8">
-			<h1 class="title">Average Query Times</h1>
+  <!-- 'on:click' below needs to be looked at later -->
+  <button class="btn btn-primary text-base ml-4" on:click={() => (formModal = true)}>Get Redis Metrics</button>
+  <Modal bind:open={formModal} size="sm" autoclose={false} class="w-full">
+    <form class="flex flex-col space-y-6">
+      <RedisForm />
+    </form>
+  </Modal>
+</div>
 
-			<!-- 'on:click' below needs to be looked at later -->
-			<button class="btn btn-primary text-base ml-4" on:click={() => (formModal = true)}>Get Redis Metrics</button>
-			<Modal bind:open={formModal} size="sm" autoclose={false} class="w-full">
-				<form class="flex flex-col space-y-6">
-					<RedisForm />
-				</form>
-			</Modal>
-		</div>
-    <div class="flex w-10/12 ml-10">
 
-      <svg {width} {height} class="all-metrics">
-        <AxisX {barChartData} {height} {xScale} {margin} />
-        <AxisY {barChartData} {width} {yScale} {margin} />
-        <g class="bars">
-          <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				{#each barChartData as data, i}
-					<rect
-          x={xScale(`${i}`)}
-          y={yScale(data.numberOfQueries)}
-          width={xScale.bandwidth()}
-						height={height - margin.top - margin.bottom - yScale(data.numberOfQueries)}
-						fill={hoveredData && hoveredData == data ? 'purple' : 'steelblue'}
-						opacity={hoveredData ? (hoveredData == data ? '1' : '.3') : '1'}
-						stroke="black"
-						on:mouseover={() => {
-              hoveredData = data;
-						}}
-						on:focus={() => {
-							hoveredData = data;
-						}}
-						tabindex="0"
-					/>
-				{/each}
+
+
+<!-- render chart -->
+<div class="h-full w-full flex space-start" style="aspect-ratio: 16/9;">
+  <svg viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="xMidYMid meet" class="h-full w-full all-metrics">
+    <g transform={`translate(${margin.left},${margin.top})`}>
+      <!-- ... your xScale ticks and other elements ... -->
+      {#each xScale.ticks() as tickValue}
+			<g transform={`translate(${xScale(tickValue)},0)`}>
+				<line y1="0" y2={innerHeight} stroke="black" />
+				<text text-anchor="middle" dy=".71em" y={innerHeight + 3}>
+					{tickValue}
+				</text>
 			</g>
-      <text
-  transform={`rotate(-90) translate(${-height / 2}, ${margin.left - 35})`}
-  dy=".71em"
-  style="text-anchor: middle; font-size: 20px;"
->
-  Number of Queries
-</text>
-		</svg>
-  </div>
-		{#if hoveredData}
-			<Tooltip data={hoveredData} {xScale} {yScale} />
-      {/if}
-    </div>
-    {:else}
-    <!-- only renders h1 when no queries exist -->
-    <div class="p-8">
-      <h1>Enter queries to see all metrics</h1>
-    </div>
-    {/if}
-    
-    <style>
-      .title {
-        display: flex;
-        justify-content: center;
-      }
-      
-      rect {
-        transition: opacity 300ms ease;
-		cursor: pointer;
+		{/each}
+        
+    {#each data as d}
+        <text text-anchor="end" x="-3" dy=".32em" y={yScale(d.queryName) + yScale.bandwidth() / 2}>
+          {d.queryName}
+        </text>
+        <rect 
+          x="0" 
+          y={yScale(d.queryName)} 
+          width={xScale(d.averageTime)} 
+          height={yScale.bandwidth()} 
+          style="fill: teal;" 
+          tabindex="0" 
+          on:mouseover={() => handleMouseOver(d, event)}
+          on:mouseout={handleMouseOut}
+          on:focus={() => handleMouseOver(d, event)} 
+          on:blur={handleMouseOut}  
+        />
+      {/each}
+
+      {#if showTooltip}
+        <rect 
+          x={tooltipX} 
+          y={tooltipY - 15}
+          width={rectWidth}
+          height="30"
+          fill="gray"
+        />
+        <text 
+          bind:this={textNode} 
+          x={tooltipX + rectWidth / 2} 
+          y={tooltipY} 
+          class="tooltip2" 
+          text-anchor="middle">
+          {tooltipContent}
+        </text>
+  {/if}
+    </g>
+  </svg>
+</div>
+  <style>
+    .all-metrics {
+		display: block;
+		width: 90vw;
+		height: auto;
 	}
 
-	rect:focus {
-		outline: none;
-	}
-
-	h1 {
-		font-size: 1.5rem;
-		font-weight: 600;
-		margin-bottom: 0.5rem;
-	}
- 
-</style>
+  .tooltip2 {
+    background-color: gray;
+    border: 1px solid white;
+    padding: 5px;
+    width:80px;
+    pointer-events: none;
+  }
+  </style>
