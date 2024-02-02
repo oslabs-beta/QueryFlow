b34efd336d8d21aacd3a1f90be36e8a304ff617d
Niko <niko8774@yahoo.com>
2024-02-02 11:47:49 -0800

    hotfix copy npm i queryflow.js had wrong text

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,40 @@
-module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2021,
-    extraFileExtensions: ['.svelte']
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
-    }
-  ],
-  // Adds rules for codesmith-esque linting
-  rules: {
-    indent: ['warn', 2],
-    '@typescript-eslint/no-unused-vars': ['off', { vars: 'local' }],
-    'prefer-const': 'warn',
-    quotes: ['warn', 'single'],
-    semi: ['warn', 'always'],
-    'space-infix-ops': 'warn',
-  },
+// module.exports = {
+//   root: true,
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:svelte/recommended',
+//     'prettier'
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['@typescript-eslint'],
+//   parserOptions: {
+//     sourceType: 'module',
+//     ecmaVersion: 2021,
+//     extraFileExtensions: ['.svelte']
+//   },
+//   env: {
+//     browser: true,
+//     es2017: true,
+//     node: true
+//   },
+//   overrides: [
+//     {
+//       files: ['*.svelte'],
+//       parser: 'svelte-eslint-parser',
+//       parserOptions: {
+//         parser: '@typescript-eslint/parser'
+//       }
+//     }
+//   ],
+//   // Adds rules for codesmith-esque linting
+//   rules: {
+//     indent: ['warn', 2],
+//     '@typescript-eslint/no-unused-vars': ['off', { vars: 'local' }],
+//     'prefer-const': 'warn',
+//     quotes: ['warn', 'single'],
+//     semi: ['warn', 'always'],
+//     'space-infix-ops': 'warn',
+//   },
 	
-};
+// };
