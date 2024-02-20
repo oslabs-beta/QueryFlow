5d0fec5fd8d77f8c9d0a222769bc89801dd80a2a
Niko <niko8774@yahoo.com>
2024-02-19 17:17:04 -0800

    updates reddis instance info

--- server/models/redisModel.mjs
+++ server/models/redisModel.mjs
@@ -3,8 +3,8 @@ import redis from 'redis';
 const redisModel = redis.createClient({
   password: process.env.REDIS_PASS,
   socket: {
-    host: 'redis-14723.c289.us-west-1-2.ec2.cloud.redislabs.com',
-    port: '14723'
+    host: 'redis-16881.c267.us-east-1-4.ec2.cloud.redislabs.com',
+    port: '16881'
   },
 });
 
